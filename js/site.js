4a3310e767ca89d61d702bcda119298c5b368efe
Joy Dutta <dutta_joy@hotmail.com>
2023-11-17 14:50:17 +0000

    Completes the project by adding the css file, js file and all the html files
    In this commit we have added the JS functionalities to display the reversed string to the user

--- js/site.js
+++ js/site.js
@@ -1,4 +1,29 @@
-// Call the alert function to alert Hello World string
-function helloWorld() {
-  alert("Hello World");
+// Get the string from the page
+// 1. Controller function
+function getValue() {
+  document.getElementById("alert").classList.add("invisible");
+  let userString = document.getElementById("inputString").value 
+  let revString = reverseInput(userString);
+  displayReversedString(revString);
 }
+
+// reverse the string 
+// 2. Logic function
+function reverseInput(text) {
+  let reversedString = ""
+  for (let index = text.length - 1; index >= 0 ; index--) {
+    reversedString += text[index];
+  }
+  return reversedString;
+}
+
+// display the reversed string to the user
+// 3. View function
+function displayReversedString(text){
+
+  document.getElementById("msg").innerHTML = `Your  string reversed is: ${text}`
+  document.getElementById("alert").classList.remove("invisible");
+}
+
+
+
